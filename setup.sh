aa265f44076ef5102b8700b5d359bc730ed63de0
Yuvraj <yuvrajsahu007@gmail.com>
2021-04-19 20:57:20 +0530

    fixed settings

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-set -a; source .env; set +a;
+file=$([ -z "$1" ] && echo ".env" || echo ".env.$1")
+
+if [ -f $file ]; then
+    echo "$file environment selected"
+    export $(cat $file | xargs)
+else 
+    echo "Not able to find the environment"
+fi
