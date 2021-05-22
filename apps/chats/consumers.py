7b60405caca873470e1ad481b02df7f96e25ac0b
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-05-22 15:39:31 +0530

    implemented token auth but need some fixes to make it work

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -1,9 +1,11 @@
 import json
-from os import sync
+from os import close, sync
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
 
 class ChatConsumer(WebsocketConsumer):
+
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -11,7 +13,6 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name, 
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self, close_code):
@@ -23,16 +24,20 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message')
+        channel_display_pic = text_data_json.get('channel_display_pic')
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'channel_display_pic' : channel_display_pic,
             }
         )
 
     def chat_message(self, event):
         message = event.get('message')
+        channel_display_pic = event.get('channel_display_pic')
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'channel_display_pic': channel_display_pic,
         }))
