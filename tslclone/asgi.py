b1bb42dec3c184f3580efdcc12e65869b7f379c8
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-05-22 21:58:14 +0530

    fix app loading for asgi application

--- tslclone/asgi.py
+++ tslclone/asgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-import os
+import django
+
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
