4d4e2cbaf320a762e3d9f54350b5628d7ff73e05
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-05-26 20:51:42 +0530

    fixed cart rest apis

--- apps/checkout/serializers.py
+++ apps/checkout/serializers.py
@@ -1,24 +1,34 @@
+from django.db.models import fields
 from apps.products.serializers import CategorySerializer
 from apps.channels.models import Channel
 from apps.products.models import Product
 from rest_framework import serializers
 from apps.checkout.models import CartItem
 
-class EditCartProductChannelSerializer(serializers.ModelSerializer):
+class CartProductChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
         fields = ('id', 'name')
 
-class EditCartProductSerializer(serializers.ModelSerializer):
-    channel = EditCartProductChannelSerializer(read_only=True)
-    category = CategorySerializer()
+class CartProductSerializer(serializers.ModelSerializer):
+    channel = CartProductChannelSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'primary_image', 'channel', 'category', 'price')
 
-class EditCartSerializer(serializers.ModelSerializer):
-    product=EditCartProductSerializer(read_only=True)
+class CartSerializer(serializers.ModelSerializer):
+    product=CartProductSerializer(read_only=True)
     class Meta:
         model = CartItem
         fields = ('id', 'product', 'quantity')
         depth = 1
+
+
+class EditCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ('id', 'product', 'quantity')
+
+
+        
--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,13 +1,29 @@
+from os import write
+
+from django.http import request
 from apps.checkout.models import CartItem
 from django.shortcuts import render
-from apps.checkout.serializers import EditCartSerializer
+from apps.checkout.serializers import CartSerializer, EditCartSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 class EditCartViewSet(viewsets.ModelViewSet):
-    serializer_class = EditCartSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         user = self.request.user
         return CartItem.objects.filter(user=user)
+    
+    def get_serializer_class(self):
+        if (self.request.method == "GET"):
+            return CartSerializer
+        return EditCartSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = EditCartSerializer(data=self.request.data)
+        serializer.is_valid()
+        cart_item = CartItem.objects.create(user=user,**serializer.validated_data)
+        cart_item.save()
+        return Response(CartSerializer(cart_item).data, status=status.HTTP_201_CREATED)
