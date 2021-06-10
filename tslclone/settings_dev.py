d6ef1be589e7b1ba99d4b88641cea5c2cf04d612
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-06-10 09:04:50 +0530

    sentry added to project for error tracking

--- tslclone/settings_dev.py
+++ tslclone/settings_dev.py
@@ -1,5 +1,7 @@
 from .settings_base import *
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DATABASES = {
     'default': {
@@ -21,3 +23,19 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_LOCATION = 'media'
 
 DEFAULT_FILE_STORAGE = 'tslclone.storages.MediaStore'
+
+SENTRY_DSN = os.environ.get('SENTRY_DSN')
+
+sentry_sdk.init(
+    dsn=os.environ.get(SENTRY_DSN),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
