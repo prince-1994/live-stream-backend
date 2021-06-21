64fc19ed62e6a75d800d33cd6288eaad679dc8ec
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-06-21 16:44:55 +0530

    channel urls refined

--- apps/channels/serializers.py
+++ apps/channels/serializers.py
@@ -7,42 +7,5 @@ from django.conf import settings
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
-        fields = ('id', 'name', 'description', 'owner', 'display_pic', 'background_pic', 'arn',)
-        read_only_fields = ('owner', 'arn', 'stream_key_arn')
-
-class EditChannelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Channel
-        fields = ('id', 'name', 'description', 'owner', 'display_pic', 'background_pic', 'arn', 'stream_key_arn')
-        read_only_fields = ('owner', 'arn', 'stream_key_arn')
-
-    # Use this method for the custom field
-    def _user(self):
-        request = self.context.get('request', None)
-        if request:
-            return request.user
-        return None
-
-    def create(self, validated_data):
-        user = self._user()
-        ivs_client = boto3.client('ivs', region_name='us-west-2')
-        alphanumeric = [character for character in validated_data.get('name') if character.isalnum()]
-        alphanumeric = "".join(alphanumeric)
-        response = ivs_client.create_channel(
-            name=alphanumeric, 
-            latencyMode='LOW',
-            type=settings.AWS_S3_CHANNEL_TYPE,
-            recordingConfigurationArn=settings.AWS_S3_RECORDING_CONFIGURATION_ARN,
-            tags={
-                'env' : settings.ENV
-            }
-        )
-        obj = Channel.objects.create(
-            owner=user,
-            arn=response.get('channel').get('arn'),
-            stream_key_arn=response.get('streamKey').get('arn'),
-            **validated_data)
-        obj.save()
-        return obj
-
-
+        fields = ('id', 'name', 'description', 'owner', 'display_pic', 'background_pic')
+        read_only_fields = ('owner',)
--- tslclone/permissions.py
+++ tslclone/permissions.py
@@ -2,19 +2,9 @@ from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 from django.contrib.auth.models import AnonymousUser
 
-class IsAuthenticatedAndOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif (type(request.user) == AnonymousUser):
-            return False
-        else :
-            return obj.owner == request.user
-
+class ReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif request.method == 'POST':
-            return type(request.user) != AnonymousUser
-        else :
-            return True
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS
