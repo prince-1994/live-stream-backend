5da822f30e681da59eaad438a3ff77b01d7de9dc
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-07-05 22:19:55 +0530

    updated profile pic location

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,13 +5,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
 
+def profile_pic_name(instance, filename):
+    return f"users/{instance.id}/profile_pic/{filename}"
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     first_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50, null=True, blank=True)
-    profile_pic = models.ImageField(default=None, null=True, blank=True)
+    profile_pic = models.ImageField(default=None, null=True, blank=True, upload_to=profile_pic_name)
     stripe_customer = models.CharField(max_length=100, null = True, blank=True)
     stripe_connected_account = models.CharField(max_length=100, null=True, blank=True)
     seller_activated = models.BooleanField(default=False)
