857e0558cf283df0abb74ca25affcfaa98c4d775
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-07-18 16:09:08 +0530

    fixed profile_pic related issue in chat consumers

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -1,75 +1,80 @@
 from apps.chats.models import Message
 from os import close, sync
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import  JsonWebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 from apps.shows.models import Show
 
 
 class ShowConsumer(JsonWebsocketConsumer):
     def chat_send_message(self, data):
-        message = data.get('message')
+        message = data.get("message")
         if self.user.is_anonymous or not message:
             print(data)
         else:
-            message = Message.objects.create(user=self.user, show=self.show, content=message)
-            first_name = self.user.first_name or "" 
+            message = Message.objects.create(
+                user=self.user, show=self.show, content=message
+            )
+            first_name = self.user.first_name or ""
             last_name = self.user.last_name or ""
-            name =  f"{first_name} {last_name}"
+            name = f"{first_name} {last_name}"
 
             data = {
-                'id' : message.id,
-                'name' : name,
-                'message' : message.content,
-                'timestamp': str(message.timestamp),
+                "id": message.id,
+                "name": name,
+                "message": message.content,
+                "timestamp": str(message.timestamp),
             }
-            if self.user.profile_pic != None:
-                data['profile_pic'] = self.user.profile_pic.url
+            if self.user.profile_pic:
+                data["profile_pic"] = self.user.profile_pic.url
 
             print(data, self.channel_layer)
             async_to_sync(self.channel_layer.group_send)(
-                self.show_ws_group_name,
-                {
-                    'type' : 'chat_message',
-                    'data' : data
-                }
+                self.show_ws_group_name, {"type": "chat_message", "data": data}
             )
-    
+
     def chat_load_messages(self, data):
-        no_of_messages = data.get("no_of_messages") 
+        no_of_messages = data.get("no_of_messages")
         last_timestamp = data.get("last_timestamp")
         if not no_of_messages or not last_timestamp:
-            self.send_json({"error" : "either no_of_messages or last_timestamp is missing"})
+            self.send_json(
+                {"error": "either no_of_messages or last_timestamp is missing"}
+            )
         elif no_of_messages > 20:
-            self.send_json({"error" : "no_of_messages can not be greater than 20"})
-        messages = self.show.messages.filter(timestamp__lt=last_timestamp).order_by('-timestamp').all()[:no_of_messages]
+            self.send_json({"error": "no_of_messages can not be greater than 20"})
+        messages = (
+            self.show.messages.filter(timestamp__lt=last_timestamp)
+            .order_by("-timestamp")
+            .all()[:no_of_messages]
+        )
         message_data = []
-        
+
         for message in messages:
             author = message.user
-            first_name = author.first_name or "" 
+            first_name = author.first_name or ""
             last_name = author.last_name or ""
-            name =  f"{first_name} {last_name}"
+            name = f"{first_name} {last_name}"
 
             data = {
-                'id' : message.id,
-                'name' : name,
-                'message' : message.content,
-                'timestamp': str(message.timestamp),
+                "id": message.id,
+                "name": name,
+                "message": message.content,
+                "timestamp": str(message.timestamp),
             }
             if author.profile_pic:
-                data['profile_pic'] = author.profile_pic.url
+                data["profile_pic"] = author.profile_pic.url
             message_data.append(data)
         # print(messages)
         self.send_json(message_data)
 
     commands = {
-        'chat_send_message' : chat_send_message,
-        'chat_load_messages' : chat_load_messages
+        "chat_send_message": chat_send_message,
+        "chat_load_messages": chat_load_messages,
     }
 
     def connect(self):
         show_id = self.scope["url_route"]["kwargs"]["show_id"]
-        self.user = self.scope['user']
+        self.user = self.scope["user"]
+        print("user", self.scope["user"])
         try:
             self.show = Show.objects.get(pk=show_id)
             self.show_ws_group_name = f"ws_{self.show.id}"
@@ -81,7 +86,6 @@ class ShowConsumer(JsonWebsocketConsumer):
             print(e)
             self.close()
 
-
     def disconnect(self, close_code):
         if self.show_ws_group_name and self.channel_name:
             async_to_sync(self.channel_layer.group_discard)(
@@ -89,17 +93,17 @@ class ShowConsumer(JsonWebsocketConsumer):
             )
 
     def receive_json(self, data):
-        command_type = data.get('command')
+        command_type = data.get("command")
         if not command_type:
-            self.send_json({'error' : 'Command type not found'})
+            self.send_json({"error": "Command type not found"})
             return
         handler = self.commands.get(command_type)
         if handler:
             handler(self, data)
         else:
-            self.send_json({"error" : "Handler not found for command type"})
-    
+            self.send_json({"error": "Handler not found for command type"})
+
     def chat_message(self, event):
-        data = event['data']
+        data = event["data"]
         print("chat_message", data)
         self.send_json(data)
