706a618f4e76c0dc5888d139bf79f4ba5cd2087a
Yuvraj Sahu <yuvrajsahu007@gmail.com>
2021-08-11 20:27:30 +0530

    not creating aws channel when running locally

--- apps/channels/serializers.py
+++ apps/channels/serializers.py
@@ -52,23 +52,27 @@ class ChannelSerializer(serializers.ModelSerializer):
         channel_count = Channel.objects.filter(owner=user).count()
         if channel_count > 0:
             raise PermissionDenied("Channel already exists")
-        ivs_client = boto3.client(
-            "ivs",
-            region_name="us-west-2",
-            aws_access_key_id=settings.AWS_IVS_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_IVS_SECRET_KEY,
-        )
-        alphanumeric = [
-            character for character in validated_data.get("name") if character.isalnum()
-        ]
-        alphanumeric = "".join(alphanumeric)
-        response = ivs_client.create_channel(
-            name=alphanumeric,
-            latencyMode="LOW",
-            type=settings.AWS_IVS_CHANNEL_TYPE,
-            recordingConfigurationArn=settings.AWS_IVS_RECORDING_CONFIGURATION_ARN,
-            tags={"env": settings.ENV},
-        )
+        if settings.ENV in ["qa", "prod"]:
+
+            ivs_client = boto3.client(
+                "ivs",
+                region_name="us-west-2",
+                aws_access_key_id=settings.AWS_IVS_ACCESS_KEY,
+                aws_secret_access_key=settings.AWS_IVS_SECRET_KEY,
+            )
+            alphanumeric = [
+                character for character in validated_data.get("name") if character.isalnum()
+            ]
+            alphanumeric = "".join(alphanumeric)
+            response = ivs_client.create_channel(
+                name=alphanumeric,
+                latencyMode="LOW",
+                type=settings.AWS_IVS_CHANNEL_TYPE,
+                recordingConfigurationArn=settings.AWS_IVS_RECORDING_CONFIGURATION_ARN,
+                tags={"env": settings.ENV},
+            )
+        else: 
+            response = {"channel" : {"arn" : ""}, "streamKey": {"arn" : ""}}
         display_pic = validated_data.pop("display_pic", None)
         background_pic = validated_data.pop("background_pic", None)
         obj = Channel.objects.create(
